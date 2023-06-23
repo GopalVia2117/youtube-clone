2e141cc670b9bb26b06a043974b5c8997370225c
Gopal Vishwakarma <gopals2117@gmail.com>
2023-06-23 15:41:58 +0530

    Initial Commit

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Height } from "@mui/icons-material";
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?part=snippet&channelId=${id}&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(0 238, 247, 1) 0%, rgba(206, 3, 184, 1) 100%, rgba(0, 212, 255, 1) 100%)",
+            zIndex: 10,
+            height: '300px',
+          }}  
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2" flexWrap="wrap" width='98vw' mx="auto">
+        {/* <Box sx={{ mr: { sm: '100px' } }}/> */}
+          <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
